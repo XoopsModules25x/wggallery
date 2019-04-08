44e5e0f473941bff59fe50c3c174a4ad320a5cdf
ggoffy <gottfried.gabor@gmail.com>
2019-04-08 12:55:44 +0200

    added setting permission for test data

--- testdata/index.php
+++ testdata/index.php
@@ -55,6 +55,12 @@ function loadSampleData()
         \Xmf\Database\TableLoad::truncateTable($table);
         \Xmf\Database\TableLoad::loadTableFromArray($table, $tabledata);
     }
+	
+	// load permissions
+	$table = 'group_permission';
+	$tabledata = \Xmf\Yaml::readWrapped($table . '.yml');
+	$mid = \Xmf\Module\Helper::getHelper($moduleDirName)->getModule()->getVar('mid');
+	loadPermissionFromArray($table, $tabledata, 'gperm_modid', $mid);
 
     //  ---  COPY test folder files ---------------
     if (is_array($configurator->copyTestFolders) && count($configurator->copyTestFolders) > 0) {
@@ -80,6 +86,12 @@ function saveSampleData()
         \Xmf\Database\TableLoad::saveTableToYamlFile($table, $table . '_' . date('Y-m-d H-i-s') . '.yml');
     }
 
+	$criteria = new \CriteriaCompo();
+	$criteria->add(new \Criteria('gperm_modid', \Xmf\Module\Helper::getHelper($moduleDirName)->getModule()->getVar('mid')));
+	$skipColumns[] = 'gperm_id';
+	\Xmf\Database\TableLoad::saveTableToYamlFile('group_permission', 'group_permission_' . date('Y-m-d H-i-s') . '.yml', $criteria, $skipColumns);
+	unset($criteria);
+	
     redirect_header('../admin/index.php', 1, constant('CO_' . $moduleDirNameUpper . '_' . 'SAMPLEDATA_SUCCESS'));
 }
 
@@ -97,4 +109,56 @@ function exportSchema()
     catch (\Exception $e) {
         exit(constant('CO_' . $moduleDirNameUpper . '_' . 'EXPORT_SCHEMA_ERROR'));
     }
+}
+
+/**
+ * loadPermissionFromArray
+ *
+ * @param string $table name of table to load without prefix
+ * @param array  $data  array of rows to insert
+ *                      Each element of the outer array represents a single table row.
+ *                      Each row is an associative array in 'column' => 'value' format.
+ *
+ * @return int number of rows inserted
+ */
+function loadPermissionFromArray($table, $data, $search, $replace)
+{
+	/** @var \XoopsDatabase */
+	$db = \XoopsDatabaseFactory::getDatabaseConnection();
+
+	$prefixedTable = $db->prefix($table);
+	$count = 0;
+	
+	$sql = 'DELETE FROM ' . $prefixedTable . ' WHERE `' . $search . '`=' . $db->quote($replace);;
+	$result = $db->queryF($sql);
+
+	foreach ($data as $row) {
+		$insertInto = 'INSERT INTO ' . $prefixedTable . ' (';
+		$valueClause = ' VALUES (';
+		$first = true;
+		foreach ($row as $column => $value) {
+			if ($first) {
+				$first = false;
+			} else {
+				$insertInto .= ', ';
+				$valueClause .= ', ';
+			}
+
+			$insertInto .= $column;
+			if ($search === $column) {
+				$valueClause .= $db->quote($replace);
+			} else {
+				$valueClause .= $db->quote($value);
+			}
+		}
+
+		$sql = $insertInto . ') ' . $valueClause . ')';
+
+		$result = $db->queryF($sql);
+		if (false !== $result) {
+			++$count;
+		}
+	}
+
+	return $count;
 }
